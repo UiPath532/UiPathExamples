9412c1eb1d51aec22e82ebf3ad88dd027335bfb8
Atul Gupta <uipathdemo655@gmail.com>
2024-08-01 12:06:48 +0530

    All Example

---
+++ Day 9/Excel Data Consolidation 1/Excel Data Consolidation 1/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace ExcelDataConsolidation
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
